9e974e3d18be6588e31959aea9eb2a780fb6ade9
LINICKER OSTROSKI MOTA <17588831626@CAMPUS.MACHADO.MCH>
2024-12-12 21:14:24 -0300

    interface UserDAO

---
+++ src/model/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.User;
+
+public interface UserDAO {
+	boolean save(User user); //criação
+	List<User> listAll(); //leitura
+	boolean update(User user); //update
+	boolean delete(User user); //delete
+}
